45f776d53632a146a6655596b34344ea678715b8
Revant-E <revantjhalani@gmail.com>
2024-07-11 23:33:32 +0530

    asda

--- test/testa.py
+++ test/testa.py
@@ -18,7 +18,7 @@ index = VectorStoreIndex.from_documents(
     documents,
 )
 preseed = "Limit yourself to only the JSON list provided and do not go out of scope and give me answer to the question: "
-postseed = '? Use the JSON entries named features and description to formulate an answer, saGive me top 5 results, if there arent 5 then give only those that are relevant and do not show what doesnt exist in the provided JSON. Give the answer in the following JSON format where product-name is the product name, and reason is the reason that product is a good fit for the question asked [{"product-name" : "", "reason" : ""}, {"product-name" : "", "reason" : ""}, etc]. Only give responses with the product-name and the reason you formulate, use no other entry as a response. Give the answer in the provided JSON format only'
+postseed = '? Use the JSON entries named features and description to formulate an answer, Give me top 5 results, if there arent 5 then give only those that are relevant and do not show what doesnt exist in the provided JSON. Give the answer in the following JSON format where product-name is the product name, and reason is the reason that product is a good fit for the question asked [{"product-name" : "", "reason" : ""}, {"product-name" : "", "reason" : ""}, etc]. Only give responses with the product-name and the reason you formulate, use no other entry as a response. Give the answer in the provided JSON format only'
 
 query_engine = index.as_query_engine()
 
