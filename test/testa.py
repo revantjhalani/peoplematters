4cd71b031c82600f5413a648e70934d7ce75c39d
Revant-E <revantjhalani@gmail.com>
2024-07-11 23:28:45 +0530

    sdsasd

--- test/testa.py
+++ test/testa.py
@@ -18,7 +18,7 @@ index = VectorStoreIndex.from_documents(
     documents,
 )
 preseed = "Limit yourself to only the JSON provided and do not go out of scope and give me answer to the question: "
-postseed = '? Give me top 5 results, if there arent 5 then give only those that are relevant and do not show what doesnt exist in the provided JSON. Give the answer in the following JSON format where product-name is the product name, and reason is the reason that product is a good fit for the question asked [{"product" : "", "reason" : ""}, {"product" : "", "reason" : ""}, etc]. Give the answer in the provided JSON format only'
+postseed = '? Give me top 5 results, if there arent 5 then give only those that are relevant and do not show what doesnt exist in the provided JSON. Give the answer in the following JSON format where product-name is the product name, and reason is the reason that product is a good fit for the question asked [{"product-name" : "", "reason" : ""}, {"product-name" : "", "reason" : ""}, etc]. Only give responses with the product-name and the reason you formulate, use no other entry as a response. Give the answer in the provided JSON format only'
 
 query_engine = index.as_query_engine()
 
