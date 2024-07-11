5c846238dbcf62dba1da5df74205d40e9a0cc7fa
Revant-E <revantjhalani@gmail.com>
2024-07-11 22:12:34 +0530

    asd

--- test/test7.py
+++ test/test7.py
@@ -29,13 +29,13 @@ chat_engine1 = index1.as_chat_engine()
 # print(response)
 
 preseed = "Limit yourself to only the JSON provided and do not go out of scope and give me answer to the question: "
-postseed = '? Give me top 5 results, if there arent 5 then give only those that are relevant and do not show what doesnt exist in the provided JSON. Give the answer in the following JSON format where product is the product name, and reason is the reason that product is a good fit for the question asked [{"product" : "", "reason" : ""}, {"product" : "", "reason" : ""}, etc]. Give the answer in the provided JSON format only'
+postseed = '? Give me top 5 results, if there arent 5 then give only those that are relevant and do not show what doesnt exist in the provided JSON. Give the answer in the following JSON format where product is the product name(the name field in the json), and reason is the reason that product is a good fit for the question asked [{"product" : "", "reason" : ""}, {"product" : "", "reason" : ""}, etc]. Give the answer in the provided JSON format only'
 
 
 
 while True:
     print("Question chat1 : ")
-    response = chat_engine1.chat(preseed + " " + input() + postseed)
+    response = chat_engine1.chat(preseed + " " + input() + " " + postseed)
     print("Answer chat1 : ")
     print(response)
     # print("Question chat2 : ")
