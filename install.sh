62910f2176312d27d6731c7791a40970e39bf826
Revant-E <revantjhalani@gmail.com>
2024-07-11 21:18:11 +0530

    asda

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
 apt update 
-apt install -y python3 python3-pip python3-dev python3-setuptools build-essential 
+apt install -y python3 python3-pip python3-dev python3-setuptools build-essential pciutils lshw tmux
 curl -fsSL https://ollama.com/install.sh | sh
-pip3 install redis wget llama-index llama-index-core llama-index-readers-file llama-index-llms-ollama llama-index-embeddings-huggingface
+pip3 install redis wget llama-index llama-index-core llama-index-readers-file llama-index-llms-ollama llama-index-embeddings-huggingface fastapi
